0a64a33ee39f7edc14db2cee35bf44d428ea1693
Eren Yeşiltepe <eren.bilkentk15@hotmail.com>
2022-12-16 01:12:19 +0300

    dfvdf
    dcs

--- logic.js
+++ logic.js
@@ -1,5 +1,5 @@
 var guitar=[
-    `<a href="https://www.youtube.com/watch?v=HulsUtMPL2Q"  target="_blank"><img src="./photos/guitar/1" alt=""></a>`,
+    `<a href="https://www.youtube.com/watch?v=HulsUtMPL2Q"  target="_blank"><img src="./photos/guitar/1.png" alt=""></a>`,
     `<a href="https://www.youtube.com/watch?v=2_A2tXEFpyQ"  target="_blank"><img src="./photos/guitar/2.png" alt=""></a>`,
     `<a href="https://www.youtube.com/watch?v=UmP-XGBqess"  target="_blank"><img src="./photos/guitar/3.png" alt=""></a>`,
     `<a href="https://www.youtube.com/watch?v=kbKy5bkHFeM"  target="_blank"><img src="./photos/guitar/4.png" alt=""></a>`,
